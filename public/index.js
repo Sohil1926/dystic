6ae594323951ae269ad10c3345d02110e604354f
andytubeee <andy.tubeee@gmail.com>
2020-08-22 16:10:49 -0400

    fixed formatting

--- public/index.js
+++ public/index.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const fs = require('fs');
 var path = require('path');
+const functions = require('firebase-functions');
 
 const app = express();
 
 app.get('/', (req, res) => {
-  //   fs.readFile('./index.html', 'utf8', (err, html) => {
-  //     if (err) {
-  //       res.status(500).send('<h1>Unexpected Error</h1>');
-  //     }
-  //     res.send(html);
-  //   });
-
   res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/timestamp', (req, res) => {
+  res.sendFile(path.join(__dirname + '/timestamp.html'));
 });
 
 const PORT_LISTENING = 3000;
@@ -20,3 +18,5 @@ const PORT_LISTENING = 3000;
 app.listen(process.env.PORT || PORT_LISTENING, () =>
   console.log('Localhost started on port: ' + PORT_LISTENING)
 );
+
+exports.app = functions.https.onRequest(app);
