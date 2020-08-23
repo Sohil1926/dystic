cb16af0715d28c1ae80ef03feaed37f10e20eb1c
Sohil1926 <61813964+Sohil1926@users.noreply.github.com>
2020-08-22 20:52:46 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -33,6 +33,17 @@ btnLog.onclick = (e) => {
     .catch(function (error) {
       alert(error);
     });
+  
+  //Sohil code
+// const db = firebase.firestore();
+  var ref = firebase.database().ref();
+
+  ref.on("value", function(snapshot) {
+     console.log(snapshot.val());
+  }, function (error) {
+     console.log("Error: " + error.code);
+  });
+  
 };
 
 btnReset.onclick = () => {
