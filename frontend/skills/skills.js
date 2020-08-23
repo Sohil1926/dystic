0b188dc219b772ac0215087d66ab99c4b2776575
andytubeee <andy.tubeee@gmail.com>
2020-08-23 10:02:06 -0400

    all objects added to doc

--- frontend/skills/skills.js
+++ frontend/skills/skills.js
@@ -17,6 +17,8 @@ var cwTxt = '';
 var expTxt = '';
 var skillTxt = '';
 var projectTxt = '';
+var involvTxt = '';
+var summaryTxt = '';
 
 const db = firebase.firestore();
 
@@ -89,34 +91,135 @@ function getObjects() {
             //   .replace(',', ' |');
           });
         });
+      db.collection('involvement')
+        .get()
+        .then(function (querySnapshot) {
+          querySnapshot.forEach(function (doc) {
+            console.log(doc.data());
+            for (x in doc.data()) {
+              involvTxt += doc.data()[x] + ' | ';
+            }
+            // document.getElementById('userSkills').innerHTML += skillTxt
+            //   .replace('and', ' ')
+            //   .replace(',', ' |')
+            //   .replace(',', ' |');
+          });
+        });
+      db.collection('summary')
+        .get()
+        .then(function (querySnapshot) {
+          querySnapshot.forEach(function (doc) {
+            console.log(doc.data());
+            for (x in doc.data()) {
+              summaryTxt += doc.data()[x] + ' | ';
+            }
+            // document.getElementById('userSkills').innerHTML += skillTxt
+            //   .replace('and', ' ')
+            //   .replace(',', ' |')
+            //   .replace(',', ' |');
+          });
+        });
     }
   });
 }
 
 btnSave.onclick = () => getObjects();
-{  
+{
   var doc = new jsPDF();
   //Education
-  eduTxt.forEach(function(edu, i){
-   doc.text(20, 10 + (i * 10), 
-        "Year: " + edu.year +
-        "Location: " + edu.location +
-        "Degree: " + edu.degree +     
-        "Minors: " + edu.minors +     
-        "Institution: " + edu.institution );
-});
- //Course
-  /*  cwTxt.forEach(function(course, i){
-   doc.text(20, 10 + (i * 10), 
-        "Course Name: " + course.name +
-        ": " + course. +
-        ": " + course. +     
-        ": " + course. +     
-        ": " + course. );
-});
-*/
-doc.save(user.uid+'_resume.pdf'); 
-  
+  eduTxt.forEach(function (edu, i) {
+    doc.text(
+      20,
+      10 + i * 10,
+      'Year: ' +
+        edu.year +
+        'Location: ' +
+        edu.location +
+        'Degree: ' +
+        edu.degree +
+        'Minors: ' +
+        edu.minors +
+        'Institution: ' +
+        edu.institution
+    );
+  });
+  //Course
+  cwTxt.forEach(function (course, i) {
+    doc.text(
+      20,
+      10 + i * 10,
+      'Course Name: ' +
+        course.name +
+        'Institution: ' +
+        course.institution +
+        'Year: ' +
+        course.year +
+        'Skills: ' +
+        course.skills
+    );
+  });
+  // Experience
+  expTxt.forEach((exp, i) => {
+    doc.text(
+      20,
+      10 + i * 10,
+      'Company: ' +
+        exp.company +
+        'Contribution: ' +
+        exp.contribution +
+        'Length: ' +
+        exp.length +
+        'Location: ' +
+        exp.location +
+        'Role:' +
+        exp.role
+    );
+  });
+  // Involvement
+  involvTxt.forEach((inv, i) => {
+    doc.text(
+      20,
+      10 + i * 10,
+      'Organization: ' +
+        inv.organization +
+        'Role: ' +
+        inv.role +
+        'Length: ' +
+        inv.length +
+        'Task: ' +
+        inv.task +
+        'College:' +
+        inv.college
+    );
+  });
+
+  // Project
+  projectTxt.forEach((pro, i) => {
+    doc.text(
+      20,
+      10 + i * 10,
+      'Title: ' +
+        pro.title +
+        'Organization: ' +
+        pro.organization +
+        'Date: ' +
+        pro.date +
+        'Contribution: ' +
+        pro.contribution
+    );
+  });
+
+  // Skills
+  skillTxt.forEach((skill, i) => {
+    doc.text(20, 10 + i * 10, 'Skills: ' + skill.skills);
+  });
+
+  // Summary
+  summaryTxt.forEach((sum, i) => {
+    doc.text(20, 10 + i * 10, 'Summary: ' + sum.summary);
+  });
+
+  doc.save(user.uid + '_resume.pdf');
 }
 
 var msg = new SpeechSynthesisUtterance();
